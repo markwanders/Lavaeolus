7d13310cb262b8783494d1d8d08ba482aafe9f6e
Mark Wanders <mndmwanders@gmail.com>
2018-01-09 21:28:25 +0100

    Split Bunq and Ethereum functionality

---
+++ src/main/java/com/example/lavaeolus/controller/EthereumController.java
@@ -0,0 +1,34 @@
+package com.example.lavaeolus.controller;
+
+import com.example.lavaeolus.controller.domain.EthereumResponse;
+import com.example.lavaeolus.service.EthereumService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RequestMapping("/api/ethereum")
+@RestController
+public class EthereumController {
+    private static final Logger LOG = LoggerFactory.getLogger(EthereumController.class);
+
+    @Autowired
+    private EthereumService ethereumService;
+
+    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity getBalance() throws IOException {
+        LOG.info("Received request on getBalance endpoint");
+
+        EthereumResponse ethereumResponse = ethereumService.createAPIResponse();
+
+        return new ResponseEntity(ethereumResponse, HttpStatus.OK);
+    }
+
+}
--- src/main/java/com/example/lavaeolus/dao/domain/CryptoCompareReply.java
+++ src/main/java/com/example/lavaeolus/dao/domain/CryptoCompareReply.java
@@ -2,20 +2,15 @@ package com.example.lavaeolus.dao.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class CryptoCompareReply {
 
     @JsonProperty
     private BigDecimal EUR;
 
-    public BigDecimal getEUR() {
-        return EUR;
-    }
-
-    public void setEUR(BigDecimal EUR) {
-        this.EUR = EUR;
-    }
 }
