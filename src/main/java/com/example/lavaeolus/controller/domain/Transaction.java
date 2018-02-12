ba4e312e81e22891ff3c2cc208b51affe687d048
Mark Wanders <mndmwanders@gmail.com>
2018-02-12 20:51:03 +0100

    Transaction datetime in epoch time

--- src/main/java/com/example/lavaeolus/controller/domain/Transaction.java
+++ src/main/java/com/example/lavaeolus/controller/domain/Transaction.java
@@ -1,9 +1,13 @@
 package com.example.lavaeolus.controller.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Data
 public class Transaction {
@@ -13,7 +17,13 @@ public class Transaction {
 
     private String currency;
 
+    @Getter(AccessLevel.NONE)
     private LocalDateTime dateTime;
 
     private String description;
+
+    @JsonProperty("dateTime")
+    long getDateTime() {
+        return dateTime.atZone(ZoneId.systemDefault()).toEpochSecond();
+    }
 }
