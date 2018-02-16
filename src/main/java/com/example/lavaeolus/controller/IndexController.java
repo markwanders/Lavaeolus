e9fcf18248364099882b3e1aeeaaafae8dbb91e5
Mark Wanders <mndmwanders@gmail.com>
2018-02-16 12:43:17 +0100

    Basic hello world web frontend

--- src/main/java/com/example/lavaeolus/controller/IndexController.java
+++ src/main/java/com/example/lavaeolus/controller/IndexController.java
@@ -1,17 +1,14 @@
 package com.example.lavaeolus.controller;
 
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/")
-@RestController
+@Controller
 public class IndexController {
 
-    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public String index() {
-        return "Lavaeolus";
+    @RequestMapping("/")
+    String index() {
+        return "index.html";
     }
 
 }
