f390d520f465494fd459b77eecbfe40ac817351c
Mark Wanders <mndmwanders@gmail.com>
2018-02-18 11:28:18 +0100

    basic login page that does nothing

--- src/main/java/com/example/lavaeolus/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/example/lavaeolus/configuration/WebSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.lavaeolus.configuration;
 
 import com.example.lavaeolus.security.AuthenticationFilter;
+import com.example.lavaeolus.security.LoginFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -21,9 +22,17 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .exceptionHandling().and()
+                .anonymous().and()
                 .authorizeRequests()
-                .antMatchers("/api/**").authenticated()
+                .antMatchers("/").permitAll()
+                .antMatchers("/service-worker.js").permitAll()
+                .antMatchers("/bower_components/**").permitAll()
+                .antMatchers("/images/**").permitAll()
+                .antMatchers("/src/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new LoginFilter("/login"), UsernamePasswordAuthenticationFilter.class);
     }
 }
---
+++ src/main/java/com/example/lavaeolus/security/LoginFilter.java
@@ -0,0 +1,22 @@
+package com.example.lavaeolus.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+    public LoginFilter(String defaultFilterProcessesUrl) {
+        super(new AntPathRequestMatcher(defaultFilterProcessesUrl));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+        return null;
+    }
+}
