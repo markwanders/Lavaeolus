6177f48705862f74414f44fe1f62743687b2da56
Mark Wanders <mndmwanders@gmail.com>
2018-12-11 17:43:05 +0100

    Add authorization header to Rabobank request

--- src/main/java/com/example/lavaeolus/client/RabobankClient.java
+++ src/main/java/com/example/lavaeolus/client/RabobankClient.java
@@ -1,15 +1,22 @@
 package com.example.lavaeolus.client;
 
+import com.example.lavaeolus.AccessTokenResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import com.example.lavaeolus.AccessTokenResponse;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 
 @Component
 public class RabobankClient {
@@ -20,11 +27,21 @@ public class RabobankClient {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${lavaeolus.root}")
+    private String root;
+
+    @Value("${rabobank.client_id}")
+    private String clientID;
+
+    @Value("${rabobank.client_secret}")
+    private String clientSecret;
+
     public String getAccessToken(String authorizationCode) {
-        String requestURL = RABOBANK_URL + "/token?code=" + authorizationCode;
+        String rabobankRedirectURI = root + "/register/account/rabobank/";
+        String requestURL = RABOBANK_URL + "/token?code=" + authorizationCode + "&grant_type=authorization_code&redirect_uri=" + URLEncoder.encode(rabobankRedirectURI);
 
         LOG.info("Sending request to {}", requestURL);
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(requestURL, null, String.class);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(requestURL, HttpMethod.POST, new HttpEntity<>(createHeaders(clientID, clientSecret)), String.class);
 
         LOG.info("Received response: {}", responseEntity);
         try {
@@ -33,5 +50,15 @@ public class RabobankClient {
             LOG.error("An error occurred mapping the JSON response to an AccessTokenResponse: ", e);
             return null;
         }
+    }
+
+    private HttpHeaders createHeaders(String username, String password){
+        return new HttpHeaders() {{
+            String auth = username + ":" + password;
+            byte[] encodedAuth = Base64.encodeBase64(
+                    auth.getBytes(Charset.forName("US-ASCII")) );
+            String authHeader = "Basic " + new String( encodedAuth );
+            set( "Authorization", authHeader );
+        }};
     }
 }
