a205655009bababf8c5a7b03048d39b85e120cd1
Mark Wanders <mndmwanders@gmail.com>
2018-12-14 15:23:50 +0100

    Added obfuscated accessToken to rabobankAccesstoken json

--- src/main/java/com/example/lavaeolus/database/domain/RabobankToken.java
+++ src/main/java/com/example/lavaeolus/database/domain/RabobankToken.java
@@ -3,6 +3,8 @@ package com.example.lavaeolus.database.domain;
 import com.example.lavaeolus.AccessTokenResponse;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,5 +17,14 @@ public class RabobankToken extends AccessTokenResponse {
     @Id
     @GeneratedValue
     private long rabobankTokenId;
+
+    @JsonProperty("accessToken")
+    public String getObfuscatedAccessToken() {
+        //Obfuscate secrets in API response
+        if (this.getAccessToken() != null && !this.getAccessToken().isEmpty()) {
+            int length = this.getAccessToken().length();
+            return StringUtils.repeat("*", Math.max(length - 4, 0)) + this.getAccessToken().substring(Math.max(length - 4, 0), length);
+        } else return null;
+    }
 
 }
